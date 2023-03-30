6cfe9526d4deb0c131e5e7d6ab09f451323f6120
Mya Thomas <mth1759@wgu.edu>
2023-03-30 17:27:17 -0600

    Establishing Application Queries

--- src/Queries/AppointmentQuery.java
+++ src/Queries/AppointmentQuery.java
@@ -1,12 +1,17 @@
 package Queries;
 
 import Models.AppointmentModel;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.time.LocalDateTime;
 
 public class AppointmentQuery {
 
-    public static boolean clashingAppointments(int appCustomerId, LocalDateTime appStart, LocalDateTime appEnd){
+    public static ObservableList<AppointmentModel> obtainAllAppointments(){
+        ObservableList<AppointmentModel> allAppList = FXCollections.observableArrayList();
+        try {
+
+        }
     }
 }
